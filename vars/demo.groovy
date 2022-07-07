75a0567766ddaed969983242f55c4e27e90d172b
Manas Gupta <gupta-manas@users.noreply.github.com>
2022-07-07 13:57:16 +0530

    Update demo.groovy

--- vars/demo.groovy
+++ vars/demo.groovy
@@ -1,3 +1,32 @@
+@GrabConfig(systemClassLoader=true)
+@Grab('com.oracle:ojdbc7:12.1.0.2')
+import groovy.sql.Sql
+
 def call(name){
   println "Hello, ${name}"
+  def url = 'jdbc:oracle:thin:@localhost:1521:xe'
+  def user = 'SYSTEM'
+  def password = 'pass'
+  def driver = 'oracle.jdbc.OracleDriver'
+  def sql = Sql.newInstance(url, user, password, driver)
+
+  // use 'sql' instance ...
+  def query= 'select * from USERS'
+  def id, about, email, userName, pass
+  sql.query(query) { resultSet ->
+  while (resultSet.next()) {
+    id = resultSet.getInt(1)
+    about = resultSet.getString('about')
+    email = resultSet.getString('about')
+    userName = resultSet.getString('user_name')
+    pass = resultSet.getString('password')
+    }
+  }
+  
+  def output= """
+    ID=$id, about=$about, email=$email, userName=$userName, pass=$pass
+  """
+    
+  println output
+  sql.close()
 }
