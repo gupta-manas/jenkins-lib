fed57b89ecf5c6935148f1ffde67b6729a4d2b06
Manas Gupta <gupta-manas@users.noreply.github.com>
2022-07-08 14:51:50 +0530

    Update demo.groovy

--- vars/demo.groovy
+++ vars/demo.groovy
@@ -1,5 +1,5 @@
 def call(name){
   println "Hello, ${name}"
-  def return_val = powershell script: "D:\Jenkins\demo.bat", returnStatus: true
+  def return_val = powershell script: "D:\\Jenkins\\demo.bat", returnStatus: true
   println return_val
 }
