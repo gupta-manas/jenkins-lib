04b900f7a252405e0268daab733264f8ef3c1015
Manas Gupta <gupta-manas@users.noreply.github.com>
2022-07-08 14:51:09 +0530

    Update demo.groovy

--- vars/demo.groovy
+++ vars/demo.groovy
@@ -1,30 +1,5 @@
-import groovy.sql.Sql
-
 def call(name){
   println "Hello, ${name}"
-  def url = 'jdbc:oracle:thin:@localhost:1521:xe'
-  def user = 'SYSTEM'
-  def password = 'pass'
-  def driver = 'oracle.jdbc.OracleDriver'
-  def sql = Sql.newInstance(url, user, password, driver)
-
-  // use 'sql' instance ...
-  def query= 'select * from USERS'
-  def id, about, email, userName, pass
-  sql.query(query) { resultSet ->
-  while (resultSet.next()) {
-    id = resultSet.getInt(1)
-    about = resultSet.getString('about')
-    email = resultSet.getString('about')
-    userName = resultSet.getString('user_name')
-    pass = resultSet.getString('password')
-    }
-  }
-  
-  def output= """
-    ID=$id, about=$about, email=$email, userName=$userName, pass=$pass
-  """
-    
-  println output
-  sql.close()
+  def return_val = powershell script: "D:\Jenkins\demo.bat", returnStatus: true
+  println return_val
 }
