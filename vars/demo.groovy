51fa554c92e1fadff67ef72b5556a8b4ebdb5de3
Manas Gupta <gupta-manas@users.noreply.github.com>
2022-07-08 17:02:38 +0530

    Update demo.groovy

--- vars/demo.groovy
+++ vars/demo.groovy
@@ -1,5 +1,5 @@
 def call(name){
   println "Hello, ${name}"
-  def return_val = bat script: "@echo off && ${env.ORACLE_HOME}\\sqlplus -s SYSTEM/pass@localhost:1521 @D:\\Jenkins\\demo.sql", returnStatus: true
+  def return_val = bat script: "@echo off && ${env.ORACLE_HOME}\\bin\\sqlplus -s SYSTEM/pass@localhost:1521 @D:\\Jenkins\\demo.sql", returnStatus: true
   println return_val
 }
